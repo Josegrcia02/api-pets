a6dbf6182f4c2375c54de6bd679901fc7a18640c
jose <josefol881@gmail.com>
2025-10-29 22:46:48 +0100

    cambios de archivos para correcto funcionamiento

--- src/main/java/com/example/despliegue_api_rest/controller/Controllerpets.java
+++ src/main/java/com/example/despliegue_api_rest/controller/Controllerpets.java
@@ -3,56 +3,46 @@ package com.example.despliegue_api_rest.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod; // IMPORT NECESARIO
+
 import com.example.despliegue_api_rest.entity.Pet;
 import com.example.despliegue_api_rest.repository.PetRepository;
 
 @RestController
 @RequestMapping("/pet")
-/**
- * En este controlador se exponen todos los endpoint referentes a Pets {@Link Pet}
- * @version 1.0
- * @author José García
- */
-@CrossOrigin(origins = {"${app.cors.allowed-origins}"})
+@CrossOrigin(
+    origins = "${app.cors.allowed-origins}",
+    allowedHeaders = "*",
+    methods = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.OPTIONS}
+)
 public class Controllerpets {
 
-    @Value("${app.cors.allowed-origins}")
-    private String allowedOrigins;
-    
     private PetRepository petRepository;
 
-    /**
-     * Constructor del controlador
-     * @param petRepository Repositorio para consultar en BBDD.
-     */
     public Controllerpets(PetRepository petRepository){
         this.petRepository = petRepository;
     }
 
-    /**
-     * Este método devuelve el listado de pets
-     * @return {@link Pet} información de cada mascota
-     */
-    @GetMapping ("/list")
+    @GetMapping("/list")
     public List<Pet> ListadoPets(){
         return petRepository.findAll();
     }
+
     @PutMapping("/adopt/{id}")
-public Pet adoptPet(@PathVariable Long id) {
-    Optional<Pet> petOpt = petRepository.findById(id);
-    if (petOpt.isPresent()) {
-        Pet pet = petOpt.get();
-        pet.setAdopt(true);
-        return petRepository.save(pet);
-    } else {
-        throw new RuntimeException("Pet not found");
+    public Pet adoptPet(@PathVariable Long id) {
+        Optional<Pet> petOpt = petRepository.findById(id);
+        if (petOpt.isPresent()) {
+            Pet pet = petOpt.get();
+            pet.setAdopt(true);
+            return petRepository.save(pet);
+        } else {
+            throw new RuntimeException("Pet not found");
+        }
     }
-}
 }
